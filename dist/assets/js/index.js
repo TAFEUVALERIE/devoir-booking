ea29b4093368b1099be248dcd5f69e4635796b8c
TAFEUVALERIE <toukamvalerie86@gmail.com>
2023-10-25 05:30:55 -0700

    all added

---
+++ dist/assets/js/index.js
@@ -0,0 +1,7 @@
+function close(){
+  document.getElementById("Menu").style.top = "-1000px";
+}
+
+function open(){
+  document.getElementById("Menu").style.top = "0px";
+}
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/**
+ * @format
+ * @type {import('tailwindcss').Config}
+ */
+
+module.exports = {
+	content: ["./dist/**/*.{html,js}"],
+	theme: {
+		extend: {},
+	},
+	plugins: [],
+};
